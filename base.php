8b50051c5f074a812efec8a775b59fc893a43414
locii <design@joomlabamboo.com>
2013-09-22 12:03:09 +1000

    It's been a long time in between commits.

--- base.php
+++ base.php
@@ -4,7 +4,7 @@
   <!--[if lt IE 7]><div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</div><![endif]-->
 
 
-
+  
 	<div id="fullwrap">
 	  	<?php // Top row
 	  	if(zen::count_position('top')) { get_template_part('templates/top'); } ?>
--- lib/scripts.php
+++ lib/scripts.php
@@ -16,8 +16,7 @@
 function roots_scripts() {
   wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
   wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/assets/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
-  wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/template.css', false, null);
-  
+  wp_enqueue_style('roots_app', get_template_directory_uri() . '/assets/css/app.css', false, null);
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
